a31230e9dff3feee2505b0caaa9390c3ac67d7be
Veronika Hillebrand <veronika.hillebrand@maibornwolff.de>
2023-03-17 15:53:32 +0100

    Basic configuration of axe

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import axe from '@axe-core/react'
 import './index.css'
+
+const isProdEnvironemnt = import.meta.env.PROD;
+
+if (!isProdEnvironemnt) {
+  axe(React, ReactDOM, 1000);
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
