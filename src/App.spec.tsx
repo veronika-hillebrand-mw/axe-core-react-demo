4003c4eec9f9c988ee1e8f52b515ef67fa6b4819
Veronika Hillebrand <veronika.hillebrand@maibornwolff.de>
2023-03-20 10:10:43 +0100

    Setup tests with vitest & react testing library

---
+++ src/App.spec.tsx
@@ -0,0 +1,22 @@
+import App from './App'
+import { render, screen, userEvent } from './tests/utils'
+
+describe('Simple working test', () => {
+  it('the title is visible', () => {
+    render(<App />)
+    expect(screen.getByText(/Vite \+ React/i)).toBeInTheDocument()
+  })
+
+  it('should increment count on click', async () => {
+    render(<App />)
+    userEvent.click(screen.getByRole('button'))
+    expect(await screen.findByText(/count is 1/i)).toBeInTheDocument()
+  })
+
+  it('uses flexbox in app header', async () => {
+    render(<App />)
+    const element = screen.getByText('Click on the Vite and React logos to learn more')
+    expect(element.className).toEqual('read-the-docs')
+    expect(getComputedStyle(element).color).toEqual('rgb(136, 136, 136)')
+  })
+})
