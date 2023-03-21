afc9367030057bf9ab8b4881f2f7a2a57898d330
Veronika Hillebrand <veronika.hillebrand@maibornwolff.de>
2023-03-21 15:04:59 +0100

    Setup cypress-axe

--- cypress/e2e/App.cy.ts
+++ cypress/e2e/App.cy.ts
@@ -1,6 +1,16 @@
+import { terminalLog } from "../support/a11y"
+
 describe('App specification', () => {
-  it('loads', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:5173/')
+    cy.injectAxe()
+  })
+
+  it('loads', () => {
     cy.contains('Vite + React')
+  })
+
+  it('Has no detectable a11y violations on load', () => {
+    cy.checkA11y(null, null, terminalLog)
   })
 })
