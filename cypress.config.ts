5d76ebdec5ec850e5573491976119a1fc633fbe3
Veronika Hillebrand <veronika.hillebrand@maibornwolff.de>
2024-05-31 16:55:48 +0200

    Revert "Fix html-validate issues"
    This reverts commit 13e09068f322634d030c3f9c584ad8803ac38485.

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,19 +1,12 @@
 import { defineConfig } from "cypress";
-import { type ConfigData } from "html-validate";
 import htmlvalidate from "cypress-html-validate/plugin";
-
-const htmlValidateConfig: ConfigData = {
-  rules: {
-    "require-sri": "off"
-  }
-}
 
 export default defineConfig({
   video: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      htmlvalidate.install(on, htmlValidateConfig);
+      htmlvalidate.install(on);
       on('task', {
         log(message) {
           console.log(message)
